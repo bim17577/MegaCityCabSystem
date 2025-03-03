0b42bcee2daeda5e2172dd75ea49f8cd39465ec3
bim17577 <159765089+bim17577@users.noreply.github.com>
2025-03-03 08:58:53 +0530

    Update the customer Dashboard and Admin Dashboard

--- Mega_City_Cab_service/src/main/java/com/icbt/controller/CustomerController.java
+++ Mega_City_Cab_service/src/main/java/com/icbt/controller/CustomerController.java
@@ -1,48 +1,58 @@
 package com.icbt.controller;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.icbt.model.Customer;
-import com.icbt.service.CustomerService;
-
-@WebServlet("/customer")
+/**
+ * Servlet implementation class CustomerController
+ */
+@WebServlet("/CustomerController")
 public class CustomerController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private CustomerService customerService;
-    
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
     public CustomerController() {
         super();
+        // TODO Auto-generated constructor stub
     }
-
 
     public void init() throws ServletException {
-        customerService = CustomerService.getInstance();
+        customerService = customerService.getInstance();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
         String action = request.getParameter("action");
         if (action == null || action.equals("list")) {
             listCustomers(request, response);
         } else if (action.equals("add")) {
             showAddForm(request, response);
         }
-    }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String action = request.getParameter("action");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
         if ("add".equals(action)) {
             addCustomer(request, response);
         } else {
-            doGet(request, response);
-        }
-    }
+
+		doGet(request, response);
+	}
+
+}
 
     private void listCustomers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
